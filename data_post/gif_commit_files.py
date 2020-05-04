885a4d43bfdb2881ca367c0453618dfec86f26bb
Yiren Wang <wildgoose@gmail.com>
2020-05-04 05:46:46 -0400

    data daily auto update

---
+++ data_post/gif_commit_files.py
@@ -0,0 +1,6 @@
+import subprocess as cmd
+
+# commit changed files on data directory
+cmd.run("git commit ./data_post -m 'data daily auto update'", check=True, shell=True)
+# push the changes to remote repository
+cmd.run("git push -u origin master -f", check=True, shell=True)
