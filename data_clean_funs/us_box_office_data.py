0b3da4900c14d67ea825ad5407f48bb3bde13730
Yiren Wang <wildgoose@gmail.com>
2020-05-08 08:56:05 -0400

    data

---
+++ data_clean_funs/us_box_office_data.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+from data_clean_funs import box_office_data as bx
+
+
+def us_box_office_scraper(year=2020):
+    # url prams...
+    year = str(year)
+    url = "https://www.boxofficemojo.com/weekend/by-year/" + year
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # create an empty data frame to hold country code info
+    column_names = ["Date",
+                    "top_10_gross",
+                    "top_10_last_week_compare",
+                    "overall_gross",
+                    "overall_last_week_compare",
+                    "releases",
+                    "#1_release",
+                    "week"]
+    box_office_table = pd.DataFrame(columns=column_names)
+
+    # loop through all trs and append inside tds to country_code data frame as rows
+    for tr in soup.find_all('tr')[1:]:
+        tds = tr.find_all('td')
+        td_list = [tds[0].text,
+                   tds[1].text,
+                   tds[2].text,
+                   tds[3].text,
+                   tds[4].text,
+                   tds[5].text,
+                   tds[6].text,
+                   tds[10].text]
+        # print(td_list)
+        # new_row = pd.Series(td_list)
+        box_office_table = box_office_table.append(
+            pd.Series(td_list, index=box_office_table.columns),
+            ignore_index=True
+        )
+    return box_office_table
+
+
+def long_weekend_drop(df):
+    """
+    drop duplicated long weekend data
+    :param df: df of mojo domestic box office by weekend
+    :return: df without duplicated weekends
+    """
+    df = df.drop_duplicates(subset=['week'], keep='last')
+    return df
+
+
+def us_box_office_cleaner(year=2020):
+    """
+    clean mojo domestic box office data:
+    1. keep only date, total_gross and week column
+    :param year: default 2020
+    :return:
+    """
+    df = us_box_office_scraper(year)
+    df = long_weekend_drop(df)
+    df = df.iloc[:, [0, 3, 7]]
+    box_office_num_list = []
+    for usd in df.iloc[:, 1]:
+        usd = usd[1:]
+        usd = usd.replace(',', '')
+        usd = int(usd)
+        box_office_num_list.append(usd)
+    df.insert(0, 'US_boxOffice_usd', box_office_num_list, True)
+
+    # add real dates
+    # get all passed sundays and make it a data frame
+    dates = bx.sunday_df(year).reset_index()
+    # the following line modifies the new index column by
+    # 1. +1 to each number
+    # 2. reverse the Series by [::-1]
+    dates['week'] = (dates['index']+1)[::-1].reset_index(drop=True)
+    df['week'] = pd.to_numeric(df['week'])
+    df = pd.merge(df, dates, left_on='week', right_on='week')
+    df.drop(['Date', 'week', 'overall_gross', 'index'], axis=1)
+    return df
---
+++ data_get/box_office_data_get.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from data_clean_funs import box_office_data as bx, us_box_office_data as usbx
+
+box_office = bx.get_all_box_office_df()
+us_box_office = pd.read_csv("data_get/alt_data_sources/us_box_office_2020_by_weekend.csv")
+box_office['date'] = pd.to_datetime(box_office['date'])
+us_box_office['date'] = pd.to_datetime(us_box_office['date'])
+box_office_full = pd.merge(box_office, us_box_office, left_on='date', right_on='date')
+
+
+# some country codes that mojo used were not following the ISO standard:
+
+# mojo_countries = bx.mojo_country_codes()
+# for index, rows in mojo_countries.iterrows():
+#     if len(rows['country_code']) > 2:
+#         print(rows['country_name'], rows['country_code'])
+#
+# # Central America XC4
+# # East Africa XKN
+# # International FOREIGN -- removed
+# # Russia/CIS XR2
+# # Serbia and Montenegro XS2
+# # Switzerland (French) XS3
+# # Switzerland (German) XS1
+
+# # sum 2 sub division of Switzerland box office
+# box_office_full['CH_boxOffice_usd'] = \
+#     box_office_full['XS1_boxOffice_usd'].fillna(0) + box_office_full['XS3_boxOffice_usd']
+# switzerland already has an independent page, therefore removed
+
+# Russia and CIS are considered russia only
+# Russia has its own page but empty, make Russia/CIS RU
+box_office_full['RU_boxOffice_usd'] = box_office_full['XR2_boxOffice_usd']
+# Serbia and Montenegro are considered as Serbia only
+box_office_full['RS_boxOffice_usd'] = box_office_full['XS2_boxOffice_usd']
+
+# drop redundant columns
+to_drop = ['XC4', 'XKN', 'XR2', 'XS2', 'XS3', 'XS1']
+for i in to_drop:
+    c = i + '_boxOffice_usd'
+    # print(c)
+    box_office_full = box_office_full.drop([c], axis=1)
+
+box_office_full.to_csv('data/box_office_full', index=False)
+
+# # test
+# plt.cla()
+# for i in box_office_full.iloc[:, 1:]:
+#     plt.plot(box_office_full.iloc[:, 0], box_office_full.loc[:, i])
