a98d3dc58baaa14203016cf4076192bcb4fb96fb
Yiren Wang <wildgoose@gmail.com>
2020-05-11 05:20:38 -0400

    project update: update map data, changed from 110m to 50m res for faster loading; merged some areas with no ISO_2 codes; update viz_gen, added base map generator.

--- altair_viz/viz_gen.py
+++ altair_viz/viz_gen.py
@@ -13,87 +13,122 @@ all_data = pd.read_csv('data/all_data.csv')
 # load geo data
 # call gis data reading functions from map_reader
 # which reads shapefile with geopandas
-base_map_path = 'data/base_map/base_map_4326.shp'
+base_map_path = 'data/base_map/base_map_50m_4326.shp'
 gdf = mapr.get_gpd_df(base_map_path, True)
+
+
+color_list = ['black', 'silver', 'gray', 'white', 'maroon', 'red', 'purple', 'fuchsia', 'green', 'lime',
+              'olive', 'yellow', 'navy', 'blue', 'teal', 'aqua', 'orange', 'aliceblue', 'antiquewhite',
+              'aquamarine', 'azure', 'beige', 'bisque', 'blanchedalmond', 'blueviolet', 'brown', 'burlywood',
+              'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan',
+              'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta',
+              'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue',
+              'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray',
+              'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'gainsboro', 'ghostwhite', 'gold',
+              'goldenrod', 'greenyellow', 'grey', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki',
+              'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan',
+              'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightgrey', 'lightpink', 'lightsalmon',
+              'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue',
+              'lightyellow', 'limegreen', 'linen', 'magenta', 'mediumaquamarine', 'mediumblue', 'mediumorchid',
+              'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise',
+              'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'oldlace',
+              'olivedrab', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred',
+              'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'rosybrown', 'royalblue', 'saddlebrown',
+              'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'skyblue', 'slateblue', 'slategray',
+              'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'thistle', 'tomato', 'turquoise', 'violet',
+              'wheat', 'whitesmoke', 'yellowgreen', 'rebeccapurple']
+
+keys = ['active', 'confirmed', 'country_code', 'death', 'iso_a2', 'name_cn', 'name_en', 'recovered', 'reg', 'sub_reg']
 
 """
 main map
 """
 # merge geo data with covid_today data
-country_map = gdf.merge(today_data, left_on='ct_code', right_on='country_code', how='outer')
+country_map = gdf.merge(today_data, left_on='iso_a2', right_on='country_code', how='outer')
 # covert geo data frame to json and extract 'feature' section
 choro_json = json.loads(country_map.to_json())
 choro_data = alt.Data(values=choro_json['features'])
 
 
-def gen_map(geo_data, color_column, tooltip, scale_type, color_scheme='bluegreen'):
+def map_generator():
     """
     Generates Toronto neighbourhoods map with building count choropleth
     :return:
     """
 
     # Add Base Layer
-    base = alt.Chart(geo_data).mark_geoshape(
+    base = alt.Chart(choro_data).mark_geoshape(
         color='#EFEFEF',  # color for countries with no data
         stroke='white',
         strokeWidth=0.5
     ).encode(
     ).properties(
-        width=800,
-        height=800
+        width=750,
+        height=750
     )
+
     # Add choropleth Layer
-    choro = alt.Chart(geo_data).mark_geoshape(
+    choro = alt.Chart(choro_data).mark_geoshape(
         stroke='white',
         strokeWidth=0.5,
         opacity=0.8
     ).encode(
-        alt.Color(color_column,
+        alt.Color('properties.active',
                   type='quantitative',
-                  scale=alt.Scale(type=scale_type,
-                                  scheme=color_scheme),
+                  scale=alt.Scale(type='symlog',
+                                  # ['linear', 'log', 'pow', 'sqrt', *'symlog', 'identity',
+                                  # 'sequential', 'time', 'utc', 'quantile', 'quantize',
+                                  # 'threshold', 'bin-ordinal', 'ordinal', 'point', 'band'],
+                                  scheme='yelloworangered'),
                   title="Active Cases",
-                  legend=alt.Legend(orient="bottom")
-                  ),
-        tooltip=tooltip
+                  legend=alt.Legend(orient="bottom",
+                                    offset=-200)
+                  )
     )
-    return base + choro
+
+    # add selector layer
+    selection = alt.selection_multi(fields=['properties.country_code'])
+    select_color = alt.condition(selection,
+                                 alt.value('black'),
+                                 alt.value('white')
+                                 )
+    select_opacity = alt.condition(selection,
+                                   if_true=alt.value(0.5),
+                                   if_false=alt.value(0)
+                                   )
+    selector = alt.Chart(choro_data).mark_geoshape(
+        stroke='white',
+        strokeWidth=0.5,
+        opacity=0
+    ).encode(
+        color=select_color,
+        opacity=select_opacity,
+        tooltip=['properties.name_en:N',
+                 'properties.active:Q',
+                 'properties.confirmed:Q',
+                 'properties.death:Q',
+                 'properties.recovered:Q'
+                 ]
+    ).add_selection(
+        selection
+    )
+
+    out = base + choro + selector
+
+    out = out.configure_view(
+        strokeWidth=0
+    )
+
+    return out
 
 
-map_1 = gen_map(geo_data=choro_data,
-                color_column='properties.active',
-                tooltip=['properties.formal_nam:N',
-                         'properties.active:Q',
-                         'properties.confirmed:Q',
-                         'properties.death:Q',
-                         'properties.recovered:Q'],
-                scale_type='symlog',
-                # ['linear', 'log', 'pow', 'sqrt', *'symlog', 'identity',
-                # 'sequential', 'time', 'utc', 'quantile', 'quantize',
-                # 'threshold', 'bin-ordinal', 'ordinal', 'point', 'band']
-                color_scheme='yelloworangered')
-map_1.save('temp/viz/test_map.html')
+main_map = map_generator()
+main_map.save('temp/viz/test_map.html')
+
 
 """
 confirmed sqrt vs search_trend
 """
-# for i in all_data.columns:
-#     print(i)
-# # country_code
-# # date
-# # confirmed
-# # death
-# # recovered
-# # box_office_full
-# # box_office_2019_mean
-# # search_trend
-# # box_office_norm
-# # confirmed_sqrt
-# # death_sqrt
-# # recovered_sqrt
-# # active_cases
-# # active_cases_sqrt
-# # country_name
 
 
 def gen_sqrt_vs_st(source=all_data):
