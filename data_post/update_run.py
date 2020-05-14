75b72618a35777bfadbea0d4b19c46f9d33c74e9
Yiren Wang <wildgoose@gmail.com>
2020-05-14 10:38:28 -0400

    project update: - resolve viz_gen can't auto run problem

--- data_post/update_run.py
+++ data_post/update_run.py
@@ -8,7 +8,7 @@ from data_post import git_update as update
 
 os.chdir('/Users/wildgoose/PycharmProjects/covid_19_CE')
 
-# cmd.run('python data_get/all_data_get.py', shell=True)
+cmd.run('python data_get/all_data_get.py', shell=True)
 cmd.run('python altair_viz/viz_gen.py', shell=True)
 
 update.auto_commit()
