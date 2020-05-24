165c1f5336d112f52bbccd1ce63167db88b2a09a
Yiren Wang <wildgoose@gmail.com>
2020-05-24 13:52:57 -0400

    project update: - add driving codes comments

--- data_post/update_run.py
+++ data_post/update_run.py
@@ -12,3 +12,6 @@ cmd.run('python data_get/all_data_get.py', shell=True)
 cmd.run('python altair_viz/viz_gen.py', shell=True)
 
 update.auto_commit()
+
+
+# $ python PycharmProjects/covid_19_CE/data_post/update_run.py
