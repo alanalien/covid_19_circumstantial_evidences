9c33dc8c55d9f1c4fb97726fab5572b823afbf70
Yiren Wang <wildgoose@gmail.com>
2020-06-11 10:00:33 -0400

    update run fix

--- data_post/update_run.py
+++ data_post/update_run.py
@@ -8,8 +8,8 @@ from data_post import git_update as update
 
 os.chdir('/Users/wildgoose/PycharmProjects/covid_19_CE')
 
-# cmd.run('python data_get/all_data_get.py', shell=True)
-# cmd.run('python altair_viz/viz_gen.py', shell=True)
+cmd.run('python data_get/all_data_get.py', shell=True)
+cmd.run('python altair_viz/viz_gen.py', shell=True)
 cmd.run('python altair_viz/html_editor.py', shell=True)
 
 update.auto_commit('')
